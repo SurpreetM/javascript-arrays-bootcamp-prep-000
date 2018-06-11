8d76fd2907991c57d0cd03580f397dd382b35141
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-11 18:34:15 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray([x], y) { 
+  [x].unshift(y)
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+  
+}
+
+function destructivelyAddElementToEndOfArray() {
+  
+}
+
+function addElementToEndOfArray() {
+  
+}
