7bd467723dba62fdb48646abce8882ec69675f87
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-06-11 18:53:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray([arr], element) { 
-  return [arr].unshift(element)
+  return element, ...arr
 }
 
 function destructivelyAddElementToBeginningOfArray() {
